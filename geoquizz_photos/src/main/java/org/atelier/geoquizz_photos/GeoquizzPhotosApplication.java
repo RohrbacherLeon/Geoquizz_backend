4b5c5cd564fd74cbf8d86fb714593a67c828c3dc
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-03-11 11:04:35 +0100

    photos API initializing

---
+++ geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/GeoquizzPhotosApplication.java
@@ -0,0 +1,13 @@
+package org.atelier.geoquizz_photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoquizzPhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoquizzPhotosApplication.class, args);
+	}
+
+}
---
+++ geoquizz_photos/src/test/java/org/atelier/geoquizz_photos/GeoquizzPhotosApplicationTests.java
@@ -0,0 +1,16 @@
+package org.atelier.geoquizz_photos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoquizzPhotosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
