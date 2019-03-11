e59b87aac89a1d2b89eb92c0179d5892bf8666da
Anthony zink <anthony.zink@outlook.fr>
2019-03-11 11:07:40 +0100

    api player

---
+++ geoquizz-player/src/main/java/org/atelier/geoquizzplayer/GeoquizzPlayerApplication.java
@@ -0,0 +1,13 @@
+package org.atelier.geoquizzplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoquizzPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoquizzPlayerApplication.class, args);
+	}
+
+}
---
+++ geoquizz-player/src/test/java/org/atelier/geoquizzplayer/GeoquizzPlayerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.atelier.geoquizzplayer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoquizzPlayerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
