ae3c41bceec79486c314d3c76ca4de17007dbe51
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-03-11 19:00:42 +0100

    implemented entities relationships + fix sql

--- geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/boundaries/SerieRepresentation.java
+++ geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/boundaries/SerieRepresentation.java
@@ -1,8 +1,19 @@
 package org.atelier.geoquizz_photos.boundaries;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.atelier.geoquizz_photos.entities.Serie;
 import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +26,29 @@ public class SerieRepresentation {
 	
 	public SerieRepresentation(SerieResource sr) {
 		this.sr = sr;
+	}
+	
+	private Resource<Serie> serieToResource(Serie serie, boolean collection){
+		Link selfLink = linkTo(SerieRepresentation.class).slash(serie.getId()).withSelfRel();
+		if(collection) {
+			Link collectionLink = linkTo(SerieRepresentation.class).withRel("collection");
+			Link photosLink = linkTo(SerieRepresentation.class).slash(serie.getId()).slash("photos").withRel("photos");
+			return new Resource<>(serie, selfLink, collectionLink, photosLink);
+		} else {
+			return new Resource<>(serie, selfLink);
+		}
+	}
+	
+	private Resources<Resource<Serie>> seriesToResource(Iterable<Serie> series){
+		Link selfLink = linkTo(SerieRepresentation.class).withSelfRel();
+		List<Resource<Serie>> serieResources = new ArrayList<Resource<Serie>>();
+		series.forEach(serie -> serieResources.add(serieToResource(serie, true)));
+		return new Resources<>(serieResources, selfLink);
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> getAllSeries() {
+		return new ResponseEntity<>(sr.findAll(), HttpStatus.OK);
 	}
 	
 }
--- geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/entities/User.java
+++ geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/entities/User.java
@@ -1,14 +1,71 @@
 package org.atelier.geoquizz_photos.entities;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User {
 
 	@Id
 	private String id;
+	private String login;
+	private String password;
+	private String token;
+	
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private Set<Photo> photos;
 	
 	User(){}
 	
+	public User(String login, String password, String token) {
+		this.login = login;
+		this.password = password;
+		this.token = token;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Set<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(Set<Photo> photos) {
+		this.photos = photos;
+	}
+		
 }
