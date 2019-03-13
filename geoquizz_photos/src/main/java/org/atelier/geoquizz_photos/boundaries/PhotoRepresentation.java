011e371056218628f8d4607ae6fa0e19a1fdbeed
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-03-13 15:02:43 +0100

    boundaries for photos

--- geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/boundaries/PhotoRepresentation.java
+++ geoquizz_photos/src/main/java/org/atelier/geoquizz_photos/boundaries/PhotoRepresentation.java
@@ -4,28 +4,39 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.atelier.geoquizz_photos.entities.Photo;
 import org.atelier.geoquizz_photos.entities.Serie;
+import org.atelier.geoquizz_photos.exceptions.BadRequest;
+import org.atelier.geoquizz_photos.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value="/photos", produces=MediaType.APPLICATION_JSON_VALUE)
 @ExposesResourceFor(Photo.class)
 public class PhotoRepresentation {
-
+	
 	private final PhotoResource pr;
+	
+	@Autowired
+	private FileStorageService fileStorageService;
 	
 	public PhotoRepresentation(PhotoResource pr) {
 		this.pr = pr;
@@ -51,7 +62,14 @@ public class PhotoRepresentation {
 	
 	@PostMapping
 	public ResponseEntity<?> postPhoto(@RequestBody Photo photo, @RequestParam("file") MultipartFile file){
-		return null;
+		return new ResponseEntity<>(fileStorageService.storeFile(file), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> endUpload(@RequestBody Photo photo){
+		photo.setId(UUID.randomUUID().toString());
+		pr.save(photo);
+		return new ResponseEntity<>(photoToResource(photo, false), HttpStatus.OK);
 	}
 	
 }
