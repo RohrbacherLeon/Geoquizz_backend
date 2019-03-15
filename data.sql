a9e8c2c8b2cafedf111ff017bc333592a3735042
Anthony zink <anthony.zink@outlook.fr>
2019-03-15 10:23:27 +0100

    docker mysql player works

--- data.sql
+++ data.sql
@@ -73,19 +73,6 @@ INSERT INTO `photos` (`id`,`description`,`longitude`, `latitude`, `url`, `token`
 INSERT INTO `parties` (`id`, `token`, `nb_photos`, `status`, `score`, `joueur`, `serie_id`) values
 ('1', "token", 0, 1, 0, "toto", '1');
 
-INSERT INTO photos values (1, "gare", 6.173900000000003, 48.6896, "gare.jpg","token", 1,1);
-INSERT INTO photos values (2, "basilique_saint_epvre", 6.179917, 48.696003, "basilique_saint_epvre.jpg","token", 1,1);
-INSERT INTO photos values (3, "hopital_central", 6.1916289, 48.6845866, "hopital_central.png","token", 1,1);
-INSERT INTO photos values (4, "kinepolis", 6.1958243, 48.6918103, "kinepolis.jpg","token", 1,1);
-INSERT INTO photos values (5, "place_carnot",  6.1774060661316526, 48.69351770490701, "place_carnot.jpg","token", 1,1);
-INSERT INTO photos values (6, "place_des_vosges", 6.18730000000005, 48.6849, "place_des_vosges.jpg","token", 1,1);
-INSERT INTO photos values (7, "place_dombasle", 6.177900000000022, 48.6916, "place_dombasle.jpg","token", 1,1);
-INSERT INTO photos values (8, "place_dstan", 6.1832861, 48.6935244, "place_stan.jpg", 1,1);
-INSERT INTO photos values (9, "rue_saint_jean", 6.180600000000027, 48.6902, "rue_saint_jean.jpg","token", 1,1);
-INSERT INTO photos values (10, "iut_nc", 6.1609476, 48.682988, "iut_nc.png","token", 1,1);
-
-INSERT INTO parties values (1, "token", 0, 1, 0, 1, "toto");
-
 INSERT INTO `partie_photo` (`partie_id`, `photo_id`) values
 ('1', '1'),
 ('1', '2');
